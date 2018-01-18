3ddd9701e300e379180725a2a218092ff2c088e8
vss414 <vss414@mail.ru>
2018-01-18 23:43:03 +0700

    add index file

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+
+function save(array $data, string $file)
+{
+    arsort($data);
+    $str = '';
+    foreach ($data as $key => $value) {
+        $str .= "| $key\t| $value\t|\n";
+    }
+    file_put_contents($file, $str);
+}
+
+$url = "https://api.zp.ru/v1/vacancies?";
+$parameters = [
+    'geo_id' => 826,
+    'add_date' => date("Y-m-d")
+];
+
+$count = 1;
+$offset = 0;
+$limit = 25;
+
+$vacancies = [];
+$rubrics = [];
+$words = [];
+
+while ($offset < $count) {
+    $parameters['offset'] = $offset;
+    $json = file_get_contents("$url" . http_build_query($parameters));
+
+    if ($json) {
+        $data = json_decode($json);
+        $count = $data->metadata->resultset->count ?? 0;
+
+        if (is_array($data->vacancies)) {
+            foreach ($data->vacancies as $vacancy) {
+
+                $headerWords = explode(' ', $vacancy->header);
+                foreach ($headerWords as $word) {
+                    if (mb_strlen($word) > 2) { // исключаю предлоги
+                        $words[mb_strtolower ($word)]++;
+                    }
+                }
+
+                foreach ($vacancy->rubrics as $rubric) {
+                    $rubrics[$rubric->title]++;
+                }
+
+                /*
+                 * По заданию не было, однако попадались вакансии с абсолютно одинаковым названием,
+                 * поэтому решил добавить и этот вариант статистики
+                 */
+                $vacancies[$vacancy->header]++;
+            }
+        }
+    }
+    $offset += $limit;
+}
+
+save($vacancies, 'vacancies.txt');
+save($rubrics, 'rubrics.txt');
+save($words, 'words.txt');
